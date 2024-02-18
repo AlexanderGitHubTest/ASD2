c7b71a48fb0dcbb550ee1378180bd421f1b707dc
AlexanderGitHubTest <94744815+AlexanderGitHubTest@users.noreply.github.com>
2024-02-19 00:42:55 +0300

    Added a report on Methods For Tree Traversal

---
+++ 03_methods_for_tree_traversal.py/methods_for_tree_traversal.py
@@ -0,0 +1,389 @@
+"""
+Methods for tree traversal.
+"""
+
+class BSTNode:
+	
+    def __init__(self, key, val, parent):
+        self.NodeKey = key # ключ узла
+        self.NodeValue = val # значение в узле
+        self.Parent = parent # родитель или None для корня
+        self.LeftChild = None # левый потомок
+        self.RightChild = None # правый потомок
+
+
+class BSTFind: # промежуточный результат поиска
+
+    def __init__(self):
+        self.Node = None # None если 
+        # в дереве вообще нету узлов
+
+        self.NodeHasKey = False # True если узел найден
+        self.ToLeft = False # True, если родительскому узлу надо 
+        # добавить новый узел левым потомком
+
+class BST:
+
+    def __init__(self, node):
+        self.Root = node # корень дерева, или None
+
+    def FindNodeByKey(self, key):
+        # ищем в дереве узел и сопутствующую информацию по ключу
+        current_node = self.Root
+        if current_node == None:
+            return BSTFind()
+        return self.FindNodeByKey_recursion(key, current_node)
+
+    def FindNodeByKey_recursion(self, key, current_node):
+        if key == current_node.NodeKey:
+            search_result = BSTFind()
+            search_result.Node = current_node
+            search_result.NodeHasKey = True
+            return search_result
+        if (    key < current_node.NodeKey
+            and current_node.LeftChild == None
+           ):
+            search_result = BSTFind()
+            search_result.Node = current_node
+            search_result.ToLeft = True
+            return search_result
+        if (    key < current_node.NodeKey
+            and current_node.LeftChild != None
+           ):
+            current_node = current_node.LeftChild
+            return self.FindNodeByKey_recursion(key, current_node)
+        if current_node.RightChild == None:
+            search_result = BSTFind()
+            search_result.Node = current_node
+            return search_result
+        current_node = current_node.RightChild
+        return self.FindNodeByKey_recursion(key, current_node)
+        
+    def AddKeyValue(self, key, val):
+        # добавляем ключ-значение в дерево
+        key_search_result = self.FindNodeByKey(key)
+        if key_search_result.Node == None:
+            self.Root = BSTNode(key, val, None)
+            return True
+        if key_search_result.NodeHasKey:
+            return False
+        if key_search_result.ToLeft:
+            key_search_result.Node.LeftChild = BSTNode(key, val, None)
+            key_search_result.Node.LeftChild.Parent = key_search_result.Node
+            return True
+        key_search_result.Node.RightChild = BSTNode(key, val, None)
+        key_search_result.Node.RightChild.Parent = key_search_result.Node
+        return True          
+  
+    def FinMinMax(self, FromNode, FindMax):
+        # ищем максимальный/минимальный ключ в поддереве
+        # возвращается объект типа BSTNode
+        if FindMax and FromNode.RightChild == None:
+            return FromNode
+        if FindMax and FromNode.RightChild != None:
+            return self.FinMinMax(FromNode.RightChild, FindMax)
+        if FromNode.LeftChild == None:
+            return FromNode
+        return self.FinMinMax(FromNode.LeftChild, FindMax)        
+	
+    def DeleteNodeByKey(self, key):
+        # удаляем узел по ключу
+        key_search_result = self.FindNodeByKey(key)
+        if (       key_search_result.Node == None 
+            or not key_search_result.NodeHasKey
+           ):
+            return False
+        if key_search_result.Node.Parent == None:
+            key_search_result_parent_type = None
+        if (    key_search_result.Node.Parent != None
+            and key_search_result.Node.Parent.LeftChild == key_search_result.Node
+           ):
+            key_search_result_parent_type = "left_child"
+        if (    key_search_result.Node.Parent != None
+            and key_search_result.Node.Parent.RightChild == key_search_result.Node
+           ):
+            key_search_result_parent_type = "right_child"
+        # key search result is leaf
+        if (    key_search_result.Node.LeftChild == None
+            and key_search_result.Node.RightChild == None
+            and key_search_result_parent_type == None
+           ):
+            self.Root = None
+            del key_search_result.Node.Parent
+            del key_search_result.Node
+            return True
+        if (    key_search_result.Node.LeftChild == None
+            and key_search_result.Node.RightChild == None
+            and key_search_result_parent_type == "left_child"
+           ):
+            key_search_result.Node.Parent.LeftChild = None
+            del key_search_result.Node.Parent
+            del key_search_result.Node
+            return True
+        if (    key_search_result.Node.LeftChild == None
+            and key_search_result.Node.RightChild == None
+           ):
+            key_search_result.Node.Parent.RightChild = None
+            del key_search_result.Node.Parent
+            del key_search_result.Node
+            return True
+        # key search result has only left branch
+        if (    key_search_result.Node.LeftChild != None
+            and key_search_result.Node.RightChild == None
+            and key_search_result_parent_type == None
+           ):
+            key_search_result.Node.LeftChild.Parent = None
+            self.Root = key_search_result.Node.LeftChild
+            del key_search_result.Node
+            return True
+        if (    key_search_result.Node.LeftChild != None
+            and key_search_result.Node.RightChild == None
+            and key_search_result_parent_type == "left_child"
+           ):
+            key_search_result.Node.Parent.LeftChild = key_search_result.Node.LeftChild
+            key_search_result.Node.LeftChild.Parent = key_search_result.Node.Parent
+            del key_search_result.Node.LeftChild
+            del key_search_result.Node.Parent
+            del key_search_result.Node
+            return True
+        if (    key_search_result.Node.LeftChild != None
+            and key_search_result.Node.RightChild == None
+           ):
+            key_search_result.Node.Parent.RightChild = key_search_result.Node.LeftChild
+            key_search_result.Node.LeftChild.Parent = key_search_result.Node.Parent
+            del key_search_result.Node.LeftChild
+            del key_search_result.Node.Parent
+            del key_search_result.Node
+            return True
+        # key search result has only right branch
+        if (    key_search_result.Node.LeftChild == None
+            and key_search_result.Node.RightChild != None
+            and key_search_result_parent_type == None
+           ):
+            key_search_result.Node.RightChild.Parent = None
+            self.Root = key_search_result.Node.RightChild
+            del key_search_result.Node.Parent
+            del key_search_result.Node
+            return True
+        if (    key_search_result.Node.LeftChild == None
+            and key_search_result.Node.RightChild != None
+            and key_search_result_parent_type == "left_child"
+           ):
+            key_search_result.Node.Parent.LeftChild = key_search_result.Node.RightChild
+            key_search_result.Node.RightChild.Parent = key_search_result.Node.Parent
+            del key_search_result.Node.RightChild
+            del key_search_result.Node.Parent
+            del key_search_result.Node
+            return True
+        if (    key_search_result.Node.LeftChild == None
+            and key_search_result.Node.RightChild != None
+           ):
+            key_search_result.Node.Parent.RightChild = key_search_result.Node.RightChild
+            key_search_result.Node.RightChild.Parent = key_search_result.Node.Parent
+            del key_search_result.Node.RightChild
+            del key_search_result.Node
+            return True
+        # key search result has left and right branches
+        if (    key_search_result.Node.RightChild.LeftChild == None
+            and key_search_result_parent_type == None
+           ):
+            key_search_result.Node.RightChild.Parent = None
+            key_search_result.Node.LeftChild.Parent = key_search_result.Node.RightChild
+            key_search_result.Node.RightChild.LeftChild = key_search_result.Node.LeftChild
+            self.Root = key_search_result.Node.RightChild
+            del key_search_result.Node
+            return True
+        if (    key_search_result.Node.RightChild.LeftChild == None
+            and key_search_result_parent_type == "left_child"
+           ):
+            key_search_result.Node.Parent.LeftChild = key_search_result.Node.RightChild
+            key_search_result.Node.RightChild.Parent = key_search_result.Node.Parent
+            key_search_result.Node.LeftChild.Parent = key_search_result.Node.RightChild
+            key_search_result.Node.RightChild.LeftChild = key_search_result.Node.LeftChild
+            del key_search_result.Node
+            return True
+        if key_search_result.Node.RightChild.LeftChild == None:
+            key_search_result.Node.Parent.RightChild = key_search_result.Node.RightChild
+            key_search_result.Node.RightChild.Parent = key_search_result.Node.Parent
+            key_search_result.Node.LeftChild.Parent = key_search_result.Node.RightChild
+            key_search_result.Node.RightChild.LeftChild = key_search_result.Node.LeftChild
+            del key_search_result.Node
+            return True      
+        return self.DeleteNodeByKey_recursion(key_search_result.Node.RightChild.LeftChild, 
+                                         key_search_result.Node,
+                                         key_search_result_parent_type
+                                        )
+
+    def DeleteNodeByKey_recursion(self, current_node, result_node, key_search_result_parent_type):
+        if (    current_node.LeftChild == None
+            and current_node.RightChild == None
+            and key_search_result_parent_type == None
+           ):
+            current_node.Parent.LeftChild = None
+            current_node.Parent = None
+            current_node.LeftChild = result_node.LeftChild
+            current_node.RightChild = result_node.RightChild
+            result_node.LeftChild.Parent = current_node
+            result_node.RightChild.Parent = current_node
+            del result_node
+            self.Root = current_node
+            return True      
+        if (    current_node.LeftChild == None
+            and current_node.RightChild == None
+            and key_search_result_parent_type == "left_child"
+           ):
+            current_node.Parent.LeftChild = None
+            current_node.Parent = result_node.Parent
+            current_node.LeftChild = result_node.LeftChild
+            current_node.RightChild = result_node.RightChild
+            result_node.Parent.LeftChild = current_node
+            result_node.LeftChild.Parent = current_node
+            result_node.RightChild.Parent = current_node
+            del result_node
+            return True      
+        if (    current_node.LeftChild == None
+            and current_node.RightChild == None
+           ):
+            current_node.Parent.LeftChild = None
+            current_node.Parent = result_node.Parent
+            current_node.LeftChild = result_node.LeftChild
+            current_node.RightChild = result_node.RightChild
+            result_node.Parent.RightChild = current_node
+            result_node.LeftChild.Parent = current_node
+            result_node.RightChild.Parent = current_node
+            del result_node
+            return True      
+        if (    current_node.LeftChild == None
+            and current_node.RightChild != None
+            and key_search_result_parent_type == None
+           ):
+            current_node.Parent.LeftChild = current_node.RightChild
+            current_node.RightChild.Parent = current_node.Parent
+            current_node.Parent = None
+            current_node.LeftChild = result_node.LeftChild
+            current_node.RightChild = result_node.RightChild
+            result_node.LeftChild.Parent = current_node
+            result_node.RightChild.Parent = current_node
+            del result_node
+            self.Root = current_node
+            return True      
+        if (    current_node.LeftChild == None
+            and current_node.RightChild != None
+            and key_search_result_parent_type == "left_child"
+           ):
+            current_node.Parent.LeftChild = current_node.RightChild
+            current_node.RightChild.Parent = current_node.Parent
+            current_node.Parent = result_node.Parent
+            current_node.LeftChild = result_node.LeftChild
+            current_node.RightChild = result_node.RightChild
+            result_node.Parent.LeftChild = current_node
+            result_node.LeftChild.Parent = current_node
+            result_node.RightChild.Parent = current_node
+            del result_node
+            return True      
+        if (    current_node.LeftChild == None
+            and current_node.RightChild != None
+           ):
+            current_node.Parent.LeftChild = current_node.RightChild
+            current_node.RightChild.Parent = current_node.Parent
+            current_node.Parent = result_node.Parent
+            current_node.LeftChild = result_node.LeftChild
+            current_node.RightChild = result_node.RightChild
+            result_node.Parent.RightChild = current_node
+            result_node.LeftChild.Parent = current_node
+            result_node.RightChild.Parent = current_node
+            del result_node
+            return True      
+        return self.DeleteNodeByKey_recursion(current_node.LeftChild, 
+                                         result_node,
+                                         key_search_result_parent_type
+                                        )
+
+    def Count(self):
+        # количество узлов в дереве
+        nodes_count = 0
+        if self.Root == None:
+            return nodes_count
+        current_node = self.Root
+        return self.Count_recursion(current_node, nodes_count)
+
+    def Count_recursion(self, current_node, nodes_count):
+        nodes_count += 1
+        if (    current_node.LeftChild == None
+            and current_node.RightChild == None
+           ):
+            return nodes_count
+        if current_node.LeftChild != None:
+            nodes_count = self.Count_recursion(current_node.LeftChild, nodes_count)
+        if current_node.RightChild != None:
+            nodes_count = self.Count_recursion(current_node.RightChild, nodes_count)
+        return nodes_count
+
+    def WideAllNodes(self):
+        # обход дерева в ширину
+        result_nodes = ()
+        if self.Root == None:
+            return result_nodes
+        current_level_nodes = (self.Root,)
+        return self.WideAllNodes_recursion(current_level_nodes, result_nodes)
+
+    def WideAllNodes_recursion(self, current_level_nodes, result_nodes):       
+        result_nodes = result_nodes + current_level_nodes        
+        new_level_nodes = []
+        for current_node in current_level_nodes:
+            if current_node.LeftChild != None:
+                new_level_nodes.append(current_node.LeftChild)
+            if current_node.RightChild != None:
+                new_level_nodes.append(current_node.RightChild)
+        if new_level_nodes == []:
+            return result_nodes
+        return self.WideAllNodes_recursion(tuple(new_level_nodes), result_nodes)
+
+    def DeepAllNodes(self, order):
+        # обход дерева в глубину
+        nodes_tuple = ()
+        if self.Root == None:
+            return nodes_tuple
+        current_node = self.Root
+        return self.DeepAllNodes_recursion(current_node, nodes_tuple, order)
+
+    def DeepAllNodes_recursion(self, current_node, nodes_tuple, order):
+        if (    current_node.LeftChild == None
+            and current_node.RightChild == None
+           ):
+            return nodes_tuple + (current_node,)
+        if order == 2:
+            nodes_tuple = nodes_tuple + (current_node,)
+        if current_node.LeftChild != None:
+            nodes_tuple = self.DeepAllNodes_recursion(current_node.LeftChild, nodes_tuple, order)
+        if order == 0:
+            nodes_tuple = nodes_tuple + (current_node,)
+        if current_node.RightChild != None:
+            nodes_tuple = self.DeepAllNodes_recursion(current_node.RightChild, nodes_tuple, order)
+        if order == 1:
+            nodes_tuple = nodes_tuple + (current_node,)
+        return nodes_tuple
+
+    def InvertTree(self):
+        # инвертация дерева
+        if self.Root == None:
+            return
+        current_node = self.Root
+        return self.InvertTree_recursion(current_node)
+
+    def InvertTree_recursion(self, current_node):
+        if (    current_node.LeftChild == None
+            and current_node.RightChild == None
+           ):
+            return
+        new_left_child = None
+        new_right_child = None
+        if current_node.LeftChild != None:
+            self.InvertTree_recursion(current_node.LeftChild)
+            new_left_child = current_node.LeftChild
+        if current_node.RightChild != None:
+            self.InvertTree_recursion(current_node.RightChild)
+            new_right_child = current_node.RightChild
+        current_node.LeftChild = new_right_child
+        current_node.RightChild = new_left_child
+
